7e34845b84b66dc1dbfb080063aaa59c3941be2a
OperalAG <80669717+operalag@users.noreply.github.com>
2021-12-17 23:59:47 +0100

    Update OperalAG.sol

--- OperalAG.sol
+++ OperalAG.sol
@@ -1,25 +1,13 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.4.0/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.4.0/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.4.0/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OperalAG is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable {
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
-
-    function initialize() initializer public {
-        __ERC20_init("OperalAG", "OAG");
-        __ERC20Burnable_init();
-        __Pausable_init();
-        __Ownable_init();
-        __ERC20Permit_init("OperalAG");
-
+contract OperalAG is ERC20, Pausable, Ownable {
+    constructor() ERC20("Operal AG", "OAG") {
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 
@@ -41,28 +29,5 @@ contract OperalAG is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
         override
     {
         super._beforeTokenTransfer(from, to, amount);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._burn(account, amount);
     }
 }
